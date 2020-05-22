27836e2154cbfacbedf7e10a5b10350df10d56f6
Beñat <benat.basabe@alumni.mondragon.edu>
2020-05-22 10:03:01 +0200

    Realizamos tranformaciones en el nif

--- Cargar Fichero.R
+++ Cargar Fichero.R
@@ -12,4 +12,18 @@ colnames(muestra08) <- c("nif", "nombre", "genero")
 
 
 #arreglamos la columna genero
- 
+
+
+
+
+#Expresiones regulares nif
+library(tidyverse)
+
+regexp <- "([[:digit:]]{8})([[:alpha:]]{1})"
+
+muestra08$genero_imp <- grepl(pattern = regexp, x = muestra08$nif)
+
+muestra08 <- muestra08%>%
+  filter(genero_imp==T)
+
+View(muestra08) 
